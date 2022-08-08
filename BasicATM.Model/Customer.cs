f5d0004c4c799f62e493c31e6547f79c99a491d9
fatihzt <muhammetfatihozata7@gmail.com>
2022-08-08 17:08:18 +0300

    Updated

---
+++ BasicATM.Model/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicATM.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Pin { get; set; }
+        public string Name { get; set; }
+        public string CardNumber { get; set; }
+        public string Username { get; set; }
+        public string Age { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int Balance { get; set; }
+        public bool IsCreditCard { get; set; }
+    }
+}
---
+++ BasicATM.Model/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicATM.Model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public TransactionType Type { get; set; }
+        public int Amount { get; set; }
+        public string TDate { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Withdraw = 1,
+        Deposit = 2,
+        FastPay= 3,
+    }
+}
